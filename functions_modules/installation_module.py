598b2dc5be1fecd314ca56fa911730b20df3e28b
moe-Ali <mohamed1234505@outlook.com>
2022-12-26 19:56:28 +0200

    adding some print logs

--- functions_modules/installation_module.py
+++ functions_modules/installation_module.py
@@ -4,11 +4,18 @@ import sys
 #function to install python packages 
 def install_python_package(package):
     system(" ".join([sys.executable,"-m","pip","install",package]))
+    print(f"Python Script: {package} installed")
     
 #main function to install all packages needed to run php into the os
 def main_install(package_manager,version,apache,sql,php):
     system(" ".join(["sudo",package_manager,"update -y"]))
+    print("Python Script: Packages Update is Done ")
+    
     system(" ".join(["sudo",package_manager,f"install -y {apache}]"]))
+    print(f"Python Script: Apache({apache}) Installed ")
+    
     system(" ".join(["sudo",package_manager,f"install -y {sql}"]))
+    print(f"Python Script: SQL({sql}) Installed ")
+    
     system(" ".join(["sudo",package_manager,f"install -y {php}{version}"]))
-    system("echo Done")
+    print(f"Python Script: PHP({php}{version}) Installed \n\n\n\n Python Script completed successfully!")
