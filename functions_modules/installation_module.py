257631b3d62d0a08b11f563e5f4357ca84137950
moe-Ali <mohamed1234505@outlook.com>
2022-12-26 20:27:53 +0200

    changing pring logs

--- functions_modules/installation_module.py
+++ functions_modules/installation_module.py
@@ -4,18 +4,18 @@ import sys
 #function to install python packages 
 def install_python_package(package):
     system(" ".join([sys.executable,"-m","pip","install",package]))
-    print(f"Python Script: {package} installed")
+    print(f"\n\nPython Script: {package} installed")
     
 #main function to install all packages needed to run php into the os
 def main_install(package_manager,version,apache,sql,php):
     system(" ".join(["sudo",package_manager,"update -y"]))
-    print("Python Script: Packages Update is Done ")
+    print("\n\nPython Script: Packages Update is Done ")
     
     system(" ".join(["sudo",package_manager,f"install -y {apache}]"]))
-    print(f"Python Script: Apache({apache}) Installed ")
+    print(f"\n\nPython Script: Apache({apache}) Installed ")
     
     system(" ".join(["sudo",package_manager,f"install -y {sql}"]))
-    print(f"Python Script: SQL({sql}) Installed ")
+    print(f"\n\nPython Script: SQL({sql}) Installed ")
     
     system(" ".join(["sudo",package_manager,f"install -y {php}{version}"]))
-    print(f"Python Script: PHP({php}{version}) Installed \n\n\n\n Python Script completed successfully!")
+    print(f"\n\nPython Script: PHP({php}{version}) Installed \n\n\n\n Python Script completed successfully!")
