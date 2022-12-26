f01a774083dd8d6a8f0d4ef185e16c7384b6c167
moe-Ali <mohamed1234505@outlook.com>
2022-12-26 18:53:01 +0200

    using the distro.name() in the main.py instade of distro.linux_distribution()

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ try:
     if platform.system() == "Linux":
         install_python_package("distro")
         import distro
-        if "Centos" in distro.linux_distribution():
+        if "Centos" in distro.name():
             main_install("yum",version)
-        elif "Ubuntu" in distro.linux_distribution():
+        elif "Ubuntu" in distro.name():
             main_install("apt",version)
         else:
             print("!Error! This Script only Supports Centos and Ubuntu")
