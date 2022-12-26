c96c0a539d26a4350b2005859725dc02790f8495
moe-Ali <mohamed1234505@outlook.com>
2022-12-26 20:32:48 +0200

    fixing the code

--- functions_modules/installation_module.py
+++ functions_modules/installation_module.py
@@ -11,7 +11,7 @@ def main_install(package_manager,version,apache,sql,php):
     system(" ".join(["sudo",package_manager,"update -y"]))
     print("\n\nPython Script: Packages Update is Done\n\n ")
     
-    system(" ".join(["sudo",package_manager,f"install -y {apache}]"]))
+    system(" ".join(["sudo",package_manager,f"install -y {apache}"]))
     print(f"\n\nPython Script: Apache({apache}) Installed\n\n ")
     
     system(" ".join(["sudo",package_manager,f"install -y {sql}"]))
